776aecf4747c185696556c478d50725cc8183556
qahramonovas <sarvinozqahramonova66@gmail.com>
2025-03-17 15:28:18 +0500

    models

--- apps/models.py
+++ apps/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.db.models import Model, CharField, ForeignKey, CASCADE
 
-# Create your models here.
+
+class Region(Model):
+    name = CharField(max_length=255)
+
+    def __str__(self):
+        return Region.name
+
+
+
+
+class District(Model):
+    name = CharField(max_length=255)
+    region = ForeignKey('apps.Region', on_delete=CASCADE, related_name='districts')
