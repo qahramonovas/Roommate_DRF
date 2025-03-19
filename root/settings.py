357863415563db4e4a47e2d876590ccfbcb9eed7
qahramonovas <sarvinozqahramonova66@gmail.com>
2025-03-19 17:55:04 +0500

    Revert "Login added"

--- root/settings.py
+++ root/settings.py
@@ -13,7 +13,6 @@ ALLOWED_HOSTS = []
 
 
 INSTALLED_APPS = [
-    'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
@@ -21,7 +20,6 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'apps',
     'rest_framework',
-    'drf_spectacular',
 ]
 
 MIDDLEWARE = [
@@ -35,8 +33,6 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'root.urls'
-AUTH_USER_MODEL = 'apps.User'
-
 
 TEMPLATES = [
     {
@@ -100,38 +96,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 #==========================
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.AllowAny'
-    ],
-    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
+        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
+    ]
 }
-
-
-
-
-
-
-SPECTACULAR_SETTINGS = {
-    'TITLE': 'Your Project API',
-    'DESCRIPTION': 'Your project description',
-    'VERSION': '1.0.0',
-    'SERVE_INCLUDE_SCHEMA': False,
-    # OTHER SETTINGS
-}
-
-
-
-
-
-
-CELERY_BROKER_URL = "redis://localhost:6379/0"
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_USE_TLS = True
-EMAIL_PORT = 587
-EMAIL_HOST_USER = "sarvinozqahramonova66@gmail.com"
-EMAIL_HOST_PASSWORD = 'nexgndvfsijlipnv'
-
-
-
-REDIS_HOST = '127.0.0.1'
-REDIS_PORT = 6379
-REDIS_DB = 1
